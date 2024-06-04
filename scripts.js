3c05993459a3c3794ca99a2864fc85115fc9f2d8
Thiago Saraiva Raposo <thiagoraposo@aluno.uema.br>
2024-06-04 14:44:36 +0000

    WikiLife Version 0.03

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
-// JavaScript para controlar a abertura e fechamento da barra lateral
 function toggleSidebar() {
   var sidebar = document.getElementById("sidebar");
+  var overlay = document.getElementById("overlay");
   if (sidebar.style.left === "0px") {
     sidebar.style.left = "-200px";
+    overlay.style.display = "none";
   } else {
     sidebar.style.left = "0px";
+    overlay.style.display = "block";
   }
 }
